7a677d215cf204b94fa16e5be130483fed2c78bf
Elliott Cheifetz <elliottsmyname@gmail.com>
2022-03-22 11:41:01 -0800

    put fetchanddisplay into its own function to increase readability down the road

--- app.js
+++ app.js
@@ -11,17 +11,21 @@ window.addEventListener('load', async () => {
   
     allCryptids = await getCryptids();
 
-    for (let cryptid of allCryptids) {
-        
+    fetchAndDisplayCryptids(allCryptids);
 
+    
+
+
+});
+
+function fetchAndDisplayCryptids(allCryptids){
+    for (let cryptid of allCryptids) {
       
         const cryptidEl = renderCryptids(cryptid);
         cryptidContainer.append(cryptidEl);
 
-    };
-
-
-});
+    }
+}
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
--- render-utils.js
+++ render-utils.js
@@ -3,13 +3,13 @@ export function renderCryptids(cryptid){
     const cryptidCardEl = document.createElement('div');
     cryptidCardEl.classList.add('list-card');
 
-    const namesEl = document.createElement('h3');
+    const namesEl = document.createElement('h4');
     const sightingsEl = document.createElement('p');
     const typeEl = document.createElement('p');
     const isDangerousEl = document.createElement('p');
 
-    namesEl.textContent = `Known as: ${cryptid.names}`;
-    sightingsEl.textContent = `Total sightings: ${cryptid.sightings}`;
+    namesEl.textContent = `${cryptid.names}`;
+    sightingsEl.textContent = `Total sightings: ${cryptid.sightings}+`;
     typeEl.textContent = `Type: ${cryptid.type}`;
     isDangerousEl.textContent = `Is Dangerous: ${cryptid.is_dangerous}`;
     
